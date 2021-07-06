45a6aeaafe99568da8daef098c97e5d2d31a91a6
Alex Lee <yul801@ucsd.edu>
2021-07-05 17:47:41 -0700

    大数乘法

--- Leetcode300/43. Multiply Strings.py
+++ Leetcode300/43. Multiply Strings.py
@@ -1,5 +1,6 @@
 class Solution:
-    def multiply(self, num1: str, num2: str) -> str:
+
+    def multiply1(self, num1: str, num2: str) -> str:
         ans = "0"
         for i in range(len(num2) - 1, -1, -1):
             one, carryIn, level = int(num2[i]), 0, ""
@@ -12,3 +13,17 @@ class Solution:
             ans = str(int(ans) + int(level))
         return ans
 
+    # 大数乘法的算法
+    def multiply(self, num1: str, num2: str) -> str:
+        n, m = len(num1), len(num2)
+        ans = [0] * (m + n)
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                mul = (ord(num1[i]) - ord("0")) * (ord(num2[j]) - ord("0"))
+                p1, p2 = i + j, i + j + 1
+                sum = mul + ans[p2]
+
+                ans[p2] = sum % 10
+                ans[p1] += sum // 10
+        ans = "".join(map(str, ans))
+        return '0' if not ans.lstrip('0') else ans.lstrip('0')
